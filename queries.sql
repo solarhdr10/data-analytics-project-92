9042d78bf72a96284df5b994fcbda1abb73cbd27
solarhdr10 <solarhdr3@duck.com>
2025-07-22 16:27:50 +0300

    fix queries again

--- queries.sql
+++ queries.sql
@@ -1,104 +1,111 @@
 -- Данный запрос вполняет функцию подсчета 
 --полей в столбце customer_id из таблицы customers
-select COUNT(customer_id) as customers_count from customers;
+SELECT COUNT(customer_id) AS customers_count FROM customers;
 
 
 -- запрос возвращает топ 10 работников 
 --по критерию дохода, с указанием количества успешных сделок
 
-select
-    CONCAT(e.first_name, ' ', e.last_name) as seller,
-    COUNT(s.sales_id) as operations,
-    FLOOR(SUM(s.quantity * p.price)) as income
-from sales as s
-left join employees as e
-    on s.sales_person_id = e.employee_id
-left join products as p
-    on s.product_id = p.product_id
-group by 1
-order by 3 desc
-limit 10;
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    COUNT(s.sales_id) AS operations,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales AS s
+LEFT JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+ORDER BY FLOOR(SUM(s.quantity * p.price)) DESC
+LIMIT 10;
 
 
 -- запрос возвращает список работников, средний доход которых ниже уровня 
 --среднего дохода среди всех работников
-select
-    CONCAT(e.first_name, ' ', e.last_name) as seller,
-    FLOOR(AVG(s.quantity * p.price)) as average_income
-from sales as s
-left join employees as e
-    on s.sales_person_id = e.employee_id
-left join products as p
-    on s.product_id = p.product_id
-group by 1
-having
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    FLOOR(AVG(s.quantity * p.price)) AS average_income
+FROM sales AS s
+LEFT JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+HAVING
     FLOOR(AVG(s.quantity * p.price))
     < (
-        select AVG(s2.quantity * p2.price)
-        from sales as s2
-        left join products as p2 on s2.product_id = p2.product_id
+        SELECT AVG(s2.quantity * p2.price)
+        FROM sales AS s2
+        LEFT JOIN products AS p2 ON s2.product_id = p2.product_id
     )
-order by 2 asc;
+ORDER BY FLOOR(AVG(s.quantity * p.price)) ASC;
 
 
 -- запрос возвращает суммарынй доход каждого сотрудника за каждый день недели
-select
-    CONCAT(e.first_name, ' ', e.last_name) as seller,
-    TRIM(TO_CHAR(s.sale_date, 'day')) as day_of_week,
-    FLOOR(SUM(s.quantity * p.price)) as income
-from sales as s
-left join products as p
-    on s.product_id = p.product_id
-left join employees as e
-    on s.sales_person_id = e.employee_id
-group by CONCAT(e.first_name, ' ', e.last_name)
-order by
-    MIN(EXTRACT(isodow from s.sale_date)) asc, 
-    CONCAT(e.first_name, ' ', e.last_name) asc;
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    TRIM(TO_CHAR(s.sale_date, 'day')) AS day_of_week,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales AS s
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+LEFT JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+GROUP BY CONCAT(e.first_name, ' ', e.last_name)
+ORDER BY
+    MIN(EXTRACT(ISODOW FROM s.sale_date)) ASC,
+    CONCAT(e.first_name, ' ', e.last_name) ASC;
 
 -- запрос, который возвращает количество клиентов в каждой возрастной категории
-select
-    case
-        when c.age between 16 and 25 then '16-25'
-        when c.age between 26 and 40 then '26-40'
-        when c.age > 40 then '40+'
-    end as age_category,
-    COUNT(c.customer_id) as age_count
-from customers as c
-group by 1
-order by 1;
+WITH customer_categories AS (
+    SELECT
+        CASE
+            WHEN c.age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN c.age BETWEEN 26 AND 40 THEN '26-40'
+            WHEN c.age > 40 THEN '40+'
+        END AS age_category
+    FROM customers AS c
+)
+
+SELECT
+    age_category,
+    COUNT(*) AS age_count
+FROM customer_categories
+GROUP BY age_category
+ORDER BY age_category;
 
 
 -- запрос возвращает колчисевто уникалных покупателей и общую выручку по месяцам
-select
-    TRIM(TO_CHAR(s.sale_date, 'yyyy-mm')) as selling_month,
-    COUNT(distinct s.customer_id) as total_customers,
-    FLOOR(SUM(s.quantity * p.price)) as income
-from sales as s
-left join products as p
-    on s.product_id = p.product_id
-group by 1
-order by 1 asc;
+SELECT
+    TRIM(TO_CHAR(s.sale_date, 'yyyy-mm')) AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM sales AS s
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY TRIM(TO_CHAR(s.sale_date, 'yyyy-mm'))
+ORDER BY TRIM(TO_CHAR(s.sale_date, 'yyyy-mm')) ASC;
 
 
 -- запрос возвращает список покупателей 
 -- первая покупка которых пришлась на время акций
-select
-    CONCAT(c.first_name, ' ', c.last_name) as customer,
-    MIN(s.sale_date) as sale_date,
-    CONCAT(e.first_name, ' ', e.last_name) as seller
-from (
-    select distinct on (s.customer_id) *
-    from sales as s
-    order by s.customer_id, s.sale_date
-) as s
-left join customers as c
-    on s.customer_id = c.customer_id
-left join employees as e
-    on s.sales_person_id = e.employee_id
-left join products as p
-    on s.product_id = p.product_id
-where p.price = 0
-group by CONCAT(c.first_name, ' ', c.last_name), 
-	s.customer_id, CONCAT(e.first_name, ' ', e.last_name)
-order by s.customer_id;
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    MIN(s.sale_date) AS sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM (
+    SELECT DISTINCT ON (s.customer_id) *
+    FROM sales AS s
+    ORDER BY s.customer_id, s.sale_date
+) AS s
+LEFT JOIN customers AS c
+    ON s.customer_id = c.customer_id
+LEFT JOIN employees AS e
+    ON s.sales_person_id = e.employee_id
+LEFT JOIN products AS p
+    ON s.product_id = p.product_id
+WHERE p.price = 0
+GROUP BY
+    CONCAT(c.first_name, ' ', c.last_name),
+    s.customer_id, CONCAT(e.first_name, ' ', e.last_name)
+ORDER BY s.customer_id;
